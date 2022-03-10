3c4987bac1c628fd98f33ef9f9fd20cde3119588
Duncan <twoontheriver@gmail.com>
2022-03-10 09:26:41 +0000

    adds author and date to comment

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,10 +1,32 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
+import * as format from "../utils/format";
 
-const CommentCard = ({ body }) => {
+const CommentCard = ({ body, author, created_at }) => {
   return (
     <>
-      <Paper variant="outlined" square sx={{ p: 1.5 }}>
+      <Paper variant="outlined" square sx={{ px: 1.5, py: 2 }}>
         <Typography> {body} </Typography>
+        <Typography
+          variant="div"
+          component="div"
+          className="card-summary"
+          sx={{ px: 0 }}
+          color="initial"
+        >
+          <Typography variant="address" component="address">
+            By{" "}
+            <a rel="author" href={`/users/${author}`}>
+              {author}
+            </a>
+          </Typography>{" "}
+          <time
+            dateTime={format.cardDate(created_at)}
+            title={format.cardDate(created_at)}
+          >
+            {format.cardDate(created_at)}
+          </time>
+          <br />
+        </Typography>
       </Paper>
     </>
   );
